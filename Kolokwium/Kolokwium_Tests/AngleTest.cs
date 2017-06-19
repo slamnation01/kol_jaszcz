790571c5cef3cc8c983b72ecd8fa9c6c1cb8c1f8
Kamil Skrzypiec <slamnation01@gmail.com>
2017-06-19 14:51:45 +0200

    Initial

---
+++ Kolokwium/Kolokwium_Tests/AngleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kolokwium_Tests
+{
+    [TestClass]
+    public class AngleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
