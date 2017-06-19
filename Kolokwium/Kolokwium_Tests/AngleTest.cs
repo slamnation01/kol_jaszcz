dedb72cc8f16a222a058b15ff16209eed05fe8fb
Kamil Skrzypiec <slamnation01@gmail.com>
2017-06-19 15:06:08 +0200

    Initial angle test + empty andle, point classes

--- Kolokwium/Kolokwium_Tests/AngleTest.cs
+++ Kolokwium/Kolokwium_Tests/AngleTest.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kolokwium;
 
 namespace Kolokwium_Tests
 {
     [TestClass]
     public class AngleTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        [TestCategory("Logic")]
+        public void CheckLine1Angle()
         {
+            double expectedAngle = 45;
+
+            Angle ang = new Angle(a, b);
+
+            if (!expectedAngle.Equals(ang.Line1Angle()))
+            {
+                Assert.Fail();
+            }
         }
     }
 }
